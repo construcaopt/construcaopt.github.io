b1739b5320d2ca1d8b0dcae620d9bc2887eba94b
construcaopt <155016655+construcaopt@users.noreply.github.com>
2024-01-11 09:22:13 +0000

    Update from WordPress on 2024-01-11 09:22:12

--- wp-content/plugins/easy-static/assets/index.js
+++ wp-content/plugins/easy-static/assets/index.js
@@ -3,12 +3,22 @@ const anchorLinks = document.querySelectorAll('a[href^="#"]'); // Select all has
 for (let item of anchorLinks) { // Loop through each link
     item.addEventListener('click', function (e) {
         let hashval = item.getAttribute('href');
-        let target = document.querySelector(hashval);
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-        history.pushState(null, null, hashval); // Change the URL hash
-        e.preventDefault();
+        if(hashval && hashval.trim().length > 0 && hashval.trim() !== '#') {
+            let target = document.querySelector(hashval);
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+            history.pushState(null, null, hashval);
+            e.preventDefault();// Change the URL hash
+        }
+    });
+}
+
+const actual_button = document.querySelectorAll('.wp-block-uagb-modal a').item(0);
+const modal_buttons = document.getElementsByClassName('open_astra_modal');
+for(let modal_button of modal_buttons){
+    modal_button.addEventListener('click',function(){
+        actual_button.click();
     });
 }
